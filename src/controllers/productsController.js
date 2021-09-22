3fa2baec802a8a5e38960b14c2f927c80a8ac9d1
MarcosZarate <agustin_8991@outlook.com>
2021-09-21 23:12:03 -0300

    controladores y ejs done

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -23,35 +23,74 @@ const controller = {
 
 	// Detail - Detail from one product
 	detail: (req, res) => {
+
+		const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
 		return res.render('detail',{
-			product : product.find(product => product.id === +req.params.id),
+			product : products.find(product => product.id === +req.params.id),
 			toDiscount,
 			toThousand
 		})
 	},
 
-	// Create - Form to create
 	create: (req, res) => {
+		
 		return res.render('product-create-form')
 	},
-	
 	// Create -  Method to store
 	store: (req, res) => {
-		return res.send(req.body)
+		const {name,price,discount,category,description} = req.body
+		let product = {
+			id: products[products.lenght -1].id +1,
+  			name: name.trim() ,
+			price: +price,
+			discount: +discount ,
+			category,
+			description: description.trim(), 
+			image: 'default-image.png'
+		}
+
+		products.push(product)
+
+		fs.writeFileSync(path.join(__dirname,'..','data','productsDataBase.json'),JSON.stringify(products,null,3),'utf-8')
+	
+		res.redirect('/products')
 	},
 
 	// Update - Form to edit
 	edit: (req, res) => {
 		// Do the magic
+		return res.render ('product-edit-form',{
+			product : products.find(product => product.id === +req.params.id)
+		})
 	},
 	// Update - Method to update
 	update: (req, res) => {
-		// Do the magic
+		const {name,price,discount,category,description} = req.body
+		let productModified = {
+			id: +req.params.id,
+  			name: name.trim() ,
+			price: +price,
+			discount: +discount ,
+			category,
+			description: description.trim(), 
+			image: 'default-image.png'
+		}
+
+		let productsModfied = products.map(product => product.id === +req.params.id ? productModified : product)
+
+		fs.writeFileSync(path.join(__dirname,'..','data','productsDataBase.json'),JSON.stringify(productsModfied,null,3),'utf-8')
+	
+		res.redirect('/products/detail/' + req.params.id)
 	},
 
 	// Delete - Delete one product from DB
 	destroy : (req, res) => {
-		// Do the magic
+		let productsModified = products.filter(product => product.id !== +req.params.id)
+		
+		fs.writeFileSync(path.join(__dirname,'..','data','productsDataBase.json'),JSON.stringify(productsModfied,null,3),'utf-8')
+
+		res.redirect('/products')
 	}
 };
 
